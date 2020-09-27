0ad1d505b71d68821102d93b7c506919d60b24c5
robert kyle armstrong <robert.armstrong.18@cnu.edu>
2020-09-27 19:56:02 -0400

    Scheduler "drafted"

--- scheduler/scheduler.cpp
+++ scheduler/scheduler.cpp
@@ -7,8 +7,28 @@
 
 //TODO fill in content
 
+void Scheduler::add{PCB p) {
+  ready_q->push(p);
+}
 
+PCB Scheduler::getNext() {
+  PCB new_process;
+  if (!(isEmpty())) {
+    new_process = ready_q.pop();
+  }
+  return new_process;
+}
 
+bool Scheduler::isEmpty() {
+  return ready_q->empty();
+}
 
-
-
+bool Scheduler::time_to_switch_processes(int tick_count, PCB &p) {
+  if (p->remaining_cpu_time == 0) { // Remainging alloted CPU time left is zero
+    return true;
+  }
+  else if (preemptive == true && (time_slice > tick_count)) { // Preemptive algorithm will be used and the current tick count is greater than allowed by time slice
+    return true;
+  }
+  return false;
+}
