e11369f330fbad20617775d263f9e2c909de94a6
robert kyle armstrong <robert.armstrong.18@cnu.edu>
2020-09-30 20:41:03 -0400

    stats.cpp and dispatcher.cpp filled out, not compiled yet.

--- stats/stats.cpp
+++ stats/stats.cpp
@@ -6,3 +6,38 @@
  */
 
 //TODO fill in content
+
+void Stats::showAllProcessInfo() {
+  vector<PCB>::iterator vec_it = vec->begin();
+  for (item in vec_it) {
+    c_out << "Process " << item.process_number << "Required CPU time:" << item.required_CPU_time << " arrived:" << item.arrival_time << " started:" << item.start_time << " finished:" << item.finish_time << endl;
+  }
+}
+
+float Stats::get_av_response_time() {
+  return av_response_time;
+}
+
+float Stats::get_av_turnaround_time() {
+  return av_turnaround_time;
+}
+
+float Stats::get_av_wait_time() {
+  return av_wait_time;
+}
+
+void Stats::calcStats() {
+    vector<PCB>::iterator vec_it = vec->begin();
+
+     // Add upp all corresponding values
+    for (item in vec_it) {
+      av_wait_time += (item.finish_time - item.arrival_time - item.required_CPU_time);
+      av_turnaround_time += (item.finish_time - item.arrival_time);
+      av_response_time += (item.start_time - item.arrival_time);
+    }
+
+    // calculate average values
+    av_wait_time = av_wait_time / vec.size();
+    av_turnaround_time = av_turnaround_time / vec.size();
+    av_response_time = av_response_time / vec.size();
+}
